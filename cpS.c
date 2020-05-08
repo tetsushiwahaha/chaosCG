d180d122b522442d2f57615c5299c23d822f46e4
Tetsushi Ueta <ueta@tokushima-u.ac.jp>
2020-05-08 13:40:32 +0900

    Update cpS.c

--- cpS.c
+++ cpS.c
@@ -1,5 +1,5 @@
 double f(double x0, double y0, double a, double b){
-	return  y0  + a * x0 * x0;
+	return  y0  + a * x0;
 }
 double g(double x0, double y0, double a, double b){
 	return  x0 * x0 + b;
